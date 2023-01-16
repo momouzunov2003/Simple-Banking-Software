4264b9a3dc3bca36b410d9b7021fd2a760ec1862
momouzunov2003 <uzunovvv03@gmail.com>
2023-01-17 01:31:19 +0200

    Adding some of the headers to the main .cpp
    Removing some functions I had written before making the headers and adding the said headers to the main .cpp

--- Banking software.cpp
+++ Banking software.cpp
@@ -1,74 +1,44 @@
 #include <iostream>
+#include "printMenus.h"
+#include "inputVerifications.h"
 #include <string>
 #include <Windows.h>
 #include <iomanip>
 #include <stdlib.h>
+#include <fstream>
 using namespace std;
 
-void clearConsole()
+void logIn()
 {
-	system("cls");
-}
-
-void printAtMiddle(string a)
-{
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	int columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	int rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-
-	int stringLength = a.length();
-
-	cout << setw(columns / 2 + stringLength / 2) << a << endl;
-}
-
-void printLogo()
-{
-	printAtMiddle("_  _ ____ _  _    ___  ____ _  _ _  _");
-	printAtMiddle("|\\/| |___ |  |    |__] |__| |\\ | |_/  ");
-	printAtMiddle("|  | |___ |__|    |__] |  | | \\| | \\_ ");
-}
-
-void printHeader()
-{
-	CONSOLE_SCREEN_BUFFER_INFO csbi;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
-	int columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
-	int rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
-	printLogo();
-	for (int i = 0; i < columns; i++)
+	cout << "Please, enter your username: ";
+	string username ;
+	USERNAME_INPUT:
+	cin >> username;
+	if (!isValidUsername(username))
 	{
-		cout << "-";
+		cout << endl << "The username must contain only latin letters or symbols!" << endl << "Re-enter your username: ";
+		goto USERNAME_INPUT;
+		cout << endl;
 	}
-	cout << endl;
-	cout << endl;
-}
-
-void printHomeMenu()
-{
-	printHeader();
-	printAtMiddle("Welcome to MEU BANK! Please, choose an option below!");
-	cout << endl << endl;
-	printAtMiddle("Press L on your keyboard to log in the bank system!");
-	cout << endl;
-	printAtMiddle("Press R on your keyboard to make a new profile in the bank system!");
-	cout << endl;
-	printAtMiddle("Press Q on your keyboard to exit from the bank system!");
-}
-
-
-void printMainMenu()
-{
-	printHeader();
-	cout << "Hello! You have X BGN. You can choose one of the folowing options: " << endl;
-	cout << "Press C to cancel your account in our bank system!" << endl;
-	cout << "Press D to make a deposit in our bank!" << endl;
-	cout << "Press L to log out from your account in our bank system!" << endl;
-	cout << "Press T to transfer money from your account to another account that exists in our banking system!" << endl;
-	cout << "Press W to withdraw money from your bank account!" << endl;
+	cout << "Please, enter your password: ";
+	//isValidLogInPassword();//da proveri direktno dali syvpada s failovata parola
 }
 
 int main()
-{
+{	
+	printHomeMenu();
+	char firstInput = ' ';
+	CHAR_INPUT:
+	cin >> firstInput;
+	if (!isValidCharInput(firstInput))
+	{
+		cout << "Please, enter a valid character:" << endl;
+		goto CHAR_INPUT;
+	}
 
+	if (firstInput == 'L' || firstInput == 'l')
+	{
+		logIn();
+	}
+	//....
 }
